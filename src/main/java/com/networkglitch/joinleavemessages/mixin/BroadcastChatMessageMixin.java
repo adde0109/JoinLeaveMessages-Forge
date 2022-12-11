8c5e0576224b1f132862d0e40f0b825070e7c783
Patrick Helms <Phelms215@gmail.com>
2022-12-11 17:23:17 -0500

    1.19.3 Update
    Version 1.19.3 support, cleaned up versioning format and moved to loom 1.0

--- src/main/java/com/networkglitch/joinleavemessages/mixin/BroadcastChatMessageMixin.java
+++ src/main/java/com/networkglitch/joinleavemessages/mixin/BroadcastChatMessageMixin.java
@@ -2,19 +2,14 @@ package com.networkglitch.joinleavemessages.mixin;
 
 
 import com.networkglitch.joinleavemessages.Joinleavemessages;
-import net.minecraft.network.message.MessageType;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
-import net.minecraft.util.registry.RegistryKey;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.UUID;
 
 @Mixin(PlayerManager.class)
 public class BroadcastChatMessageMixin {
