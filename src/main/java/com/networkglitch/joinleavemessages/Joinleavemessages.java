324eb01a48c7d4b14b5aa78e7619d425f9aadf30
Adrian Bergqvist <adde0109@gmail.com>
2023-11-16 10:44:43 +0100

    Forge port

--- src/main/java/com/networkglitch/joinleavemessages/Joinleavemessages.java
+++ src/main/java/com/networkglitch/joinleavemessages/Joinleavemessages.java
@@ -1,30 +1,23 @@
 package com.networkglitch.joinleavemessages;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Joinleavemessages implements ModInitializer {
+@Mod("joinleavemessages")
+public class Joinleavemessages {
 
     public static Logger LOGGER = LogManager.getLogger();
 
     public static final String MOD_NAME = "JoinLeaveMessages";
 
-    @Override
-    public void onInitialize() {
+
+    public Joinleavemessages() {
         log(Level.INFO, "Initializing...");
     }
 
     public static void log(Level level, String message) {
         LOGGER.log(level, "[" + MOD_NAME + "] " + message);
-    }
-
-    public static boolean isNotNull(Object o) {
-        return o != null;
-    }
-
-    public static boolean isNull(Object o) {
-        return o == null;
     }
 }
--- src/main/java/com/networkglitch/joinleavemessages/mixin/BroadcastChatMessageMixin.java
+++ src/main/java/com/networkglitch/joinleavemessages/mixin/BroadcastChatMessageMixin.java
@@ -2,29 +2,29 @@ package com.networkglitch.joinleavemessages.mixin;
 
 
 import com.networkglitch.joinleavemessages.Joinleavemessages;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.server.players.PlayerList;
 import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerManager.class)
+@Mixin(PlayerList.class)
 public class BroadcastChatMessageMixin {
 
-    @Inject(at = @At("HEAD"), method = "broadcast(Lnet/minecraft/text/Text;Z)V", cancellable = true)
-    void filterBroadCastMessages(Text message, boolean overlay, CallbackInfo ci) {
+    @Inject(at = @At("HEAD"), method = "broadcastSystemMessage(Lnet/minecraft/network/chat/Component;Z)V", cancellable = true)
+    void filterBroadCastMessages(Component message, boolean overlay, CallbackInfo ci) {
 
-        String Key = ((TranslatableTextContent) message.getContent()).getKey();
+        String Key = ((TranslatableContents) message.getContents()).getKey();
         if (
                 Key.equals("multiplayer.player.joined.renamed") ||
                         Key.equals("multiplayer.player.joined") ||
                         Key.equals("multiplayer.player.left")
         ) {
 
-            Joinleavemessages.log(Level.INFO, "Suppressing a join/leave message");
+            //Joinleavemessages.log(Level.INFO, "Suppressing a join/leave message");
             ci.cancel();
         }
     }
